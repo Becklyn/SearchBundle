e51a761d1f20abccb1d31a02a27e2413ad50077d
Jannik Zschiesche <jz@becklyn.com>
2016-12-21 00:53:57 +0100

    Register command as a service

--- Command/IndexCommand.php
+++ Command/IndexCommand.php
@@ -2,21 +2,104 @@
 
 namespace Becklyn\SearchBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Becklyn\SearchBundle\Index\IndexMapping;
+use Becklyn\SearchBundle\Loader\EntityLoader;
+use Becklyn\SearchBundle\Metadata\Metadata;
+use Becklyn\SearchBundle\Metadata\MetadataGenerator;
+use Becklyn\SearchBundle\Search\SearchIndexer;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 
-class IndexCommand extends ContainerAwareCommand
+class IndexCommand extends Command
 {
+    /**
+     * @var Metadata
+     */
+    private $metadata;
+
+
+    /**
+     * @var MetadataGenerator
+     */
+    private $metadataGenerator;
+
+
+    /**
+     * @var KernelInterface
+     */
+    private $kernel;
+
+
+    /**
+     * @var IndexMapping
+     */
+    private $indexMapping;
+
+
+    /**
+     * @var SearchIndexer
+     */
+    private $indexer;
+
+
+    /**
+     * @var EntityLoader
+     */
+    private $entityLoader;
+
+
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+
+
+    /**
+     * @param Metadata          $metadata
+     * @param MetadataGenerator $metadataGenerator
+     * @param KernelInterface   $kernel
+     * @param IndexMapping      $indexMapping
+     * @param SearchIndexer     $indexer
+     * @param EntityLoader      $entityLoader
+     * @param Registry          $doctrine
+     *
+     * @internal param MetadataGenerator $generator
+     */
+    public function __construct (
+        Metadata $metadata,
+        MetadataGenerator $metadataGenerator,
+        KernelInterface $kernel,
+        IndexMapping $indexMapping,
+        SearchIndexer $indexer,
+        EntityLoader $entityLoader,
+        Registry $doctrine
+    )
+    {
+        parent::__construct("becklyn:search:index");
+
+        $this->metadata = $metadata;
+        $this->metadataGenerator = $metadataGenerator;
+        $this->kernel = $kernel;
+        $this->indexMapping = $indexMapping;
+        $this->indexer = $indexer;
+        $this->entityLoader = $entityLoader;
+        $this->entityManager = $doctrine->getManager();
+    }
+
+
     /**
      * @inheritdoc
      */
     protected function configure()
     {
         $this
-            ->setName('becklyn:search:index')
             ->setDescription('(Re-)Indexes all app entities into ElasticSearch');
     }
 
@@ -29,7 +112,10 @@ class IndexCommand extends ContainerAwareCommand
 
         $io->title("Regenerating ElasticSearch Indexes");
 
+        $this->clearMetaData($io);
         $this->refreshMetaData($io);
+        $this->regenerateIndex($io);
+        $this->indexAllEntities($io);
 
         $io->success("All done.");
     }
@@ -50,26 +136,34 @@ class IndexCommand extends ContainerAwareCommand
 
 
     /**
+     * Clears all existing metadata
+     *
+     * @param SymfonyStyle $io
+     */
+    private function clearMetaData (SymfonyStyle $io)
+    {
+        $io->section("Clear existing metadata");
+
+        $this->metadata->clear();
+
+        $this->stepDone($io);
+    }
+
+
+    /**
      * Refreshes the meta data
      *
      * @param SymfonyStyle $io
      */
     private function refreshMetaData (SymfonyStyle $io)
     {
-        $metadata = $this->getContainer()->get("becklyn.search.metadata");
-        $metadataGenerator = $this->getContainer()->get("becklyn.search.metadata.generator");
-        $kernel = $this->getContainer()->get("kernel");
-
         $io->section("Refreshing the metadata");
 
-        $metadata->clear();
-        $io->text("Clear metadata");
-
-        foreach ($kernel->getBundles() as $bundle)
+        foreach ($this->kernel->getBundles() as $bundle)
         {
             $bundleNamespacePrefix = "{$bundle->getName()}\\";
 
-            $searchItems = $metadataGenerator->rebuildMetadata([
+            $searchItems = $this->metadataGenerator->rebuildMetadata([
                 $bundleNamespacePrefix => $bundle->getPath(),
             ]);
 
@@ -87,6 +181,66 @@ class IndexCommand extends ContainerAwareCommand
 
                 $io->newLine();
             }
+        }
+
+        $this->stepDone($io);
+    }
+
+
+
+    /**
+     * Regenerates all indexes
+     *
+     * @param SymfonyStyle $io
+     */
+    private function regenerateIndex (SymfonyStyle $io)
+    {
+        $io->section("Refreshing the index mapping");
+        $this->indexMapping->regenerateIndex();
+
+        $this->stepDone($io);
+    }
+
+
+
+    /**
+     * Reindexes all entities
+     *
+     * @param SymfonyStyle $io
+     */
+    private function indexAllEntities (SymfonyStyle $io)
+    {
+        $io->section("Indexing all entities");
+
+        foreach ($this->metadata->getAllItems() as $item)
+        {
+            $io->writeln(sprintf(
+                "<fg=blue>%s</>",
+                $item->getFqcn()
+            ));
+
+            $entities = $this->entityLoader->loadEntities($item, EntityLoader::LOAD_ALL_ENTITIES);
+            $count = count($entities);
+
+            if (0 === $count)
+            {
+                $io->writeln("  No entities found.");
+            }
+            else
+            {
+                $this->indexer->bulkIndex($entities);
+
+                $io->writeln(sprintf(
+                    "  <fg=green>%d %s indexed</>",
+                    $count,
+                    1 === $count ? "entity" : "entities"
+                ));
+
+                $entities = null;
+                $this->entityManager->clear();
+            }
+
+            $io->newLine();
         }
 
         $this->stepDone($io);
