b19adda5bc7adc69aee497f0432f1f805bb80013
Jannik Zschiesche <jz@becklyn.com>
2016-12-21 00:57:04 +0100

    Add doctrine integration

---
+++ Doctrine/DoctrineListener.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Becklyn\SearchBundle\Doctrine;
+
+use Becklyn\SearchBundle\Entity\SearchableEntityInterface;
+use Becklyn\SearchBundle\Metadata\Metadata;
+use Becklyn\SearchBundle\Search\SearchIndexer;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+
+/**
+ * Handles the automatic index update on doctrine events
+ */
+class DoctrineListener implements EventSubscriber
+{
+    /**
+     * @var Metadata
+     */
+    private $metadata;
+
+
+    /**
+     * @var SearchIndexer
+     */
+    private $indexer;
+
+
+
+    /**
+     * @param Metadata      $metadata
+     * @param SearchIndexer $indexer
+     */
+    public function __construct (Metadata $metadata, SearchIndexer $indexer)
+    {
+        $this->metadata = $metadata;
+        $this->indexer = $indexer;
+    }
+
+
+
+    /**
+     * Event handler for the "postPersist" event
+     *
+     * @param LifecycleEventArgs $args
+     */
+    public function postPersist (LifecycleEventArgs $args)
+    {
+        $this->handleDoctrineEvent($args);
+    }
+
+
+
+    /**
+     * Event handler for the "postUpdate" event
+     *
+     * @param LifecycleEventArgs $args
+     */
+    public function postUpdate (LifecycleEventArgs $args)
+    {
+        $this->handleDoctrineEvent($args);
+    }
+
+
+
+    /**
+     * Handles all doctrine events
+     *
+     * @param LifecycleEventArgs $args
+     */
+    public function handleDoctrineEvent (LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        foreach ($this->metadata->getAllItems() as $item)
+        {
+            if (is_a($entity, $item->getFqcn()))
+            {
+                /** @var SearchableEntityInterface $entity */
+                $this->indexer->index($entity);
+                return;
+            }
+        }
+
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function getSubscribedEvents ()
+    {
+        return [
+            "postPersist",
+            "postUpdate",
+        ];
+    }
+}
